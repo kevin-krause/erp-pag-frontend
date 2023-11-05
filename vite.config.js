364df07769113eb128669f8a9fbb29d4e900926d
kevin-krause <contatokevinkrause@gmail.com>
2023-11-05 00:07:42 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 // export default defineConfig({
