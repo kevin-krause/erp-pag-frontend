b4d513d004f0560e7992f9a1a5fc57df81efb1eb
kevin-krause <contatokevinkrause@gmail.com>
2023-11-05 00:09:41 -0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-/* eslint-disable no-unused-vars */
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//     server: {
-//         proxy: {
-//             '/api': {
-//                 target: 'https://api-pagani-932e9d7b1daf.herokuapp.com',
-//                 secure: false
-//             }
-//         }
-//     },
-//     plugins: [react()]
-// })
+export default defineConfig({
+    server: {
+        proxy: {
+            '/kkkkk': {
+                target: 'https://api-pagani-932e9d7b1daf.herokuapp.com',
+                secure: false
+            }
+        }
+    },
+    plugins: [react()]
+})
