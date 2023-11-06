3a3d1d54601671e85c0fa0e23365d3c7fde9d955
kevin-krause <contatokevinkrause@gmail.com>
2023-11-06 08:18:29 -0300

    check user state and sent login page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+    const navigate = useNavigate()
+    const user = useSelector(state => state.user)
+
+    useEffect(() => {
+        user ? console.log('ok') : navigate('/signin')
+    })
     return (
         <div className="p-3 border border-slate-200 w-fit rounded-lg m-3">
-            <h1 className='text-lg p-2 bg-slate-200 rounded-md text-slate-700'>Telas mais usadas</h1>
-            <ul className='p-2 text-slate-700'>
+            <h1 className="text-lg p-2 bg-slate-200 rounded-md text-slate-700">
+                Telas mais usadas
+            </h1>
+            <ul className="p-2 text-slate-700">
                 <li>
                     <Link to={'/entrances'}>Ordem de serviço</Link>
                 </li>
